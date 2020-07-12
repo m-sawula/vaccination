efa10628964539a075cfe3531daf852595877ed9
m-sawula <marek.sawula@gmail.com>
2020-07-12 09:34:41 +0200

    added models for HepatitisB

--- vax/models.py
+++ vax/models.py
@@ -19,3 +19,44 @@ class Child(models.Model):
     sex = models.CharField(max_length=1, choices=SEX)
     parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
+
+
+class Tuberculosis(models.Model):
+    expected_vax_date_1_day = models.DateField()
+    vax_date_1_day = models.DateTimeField()  # hour are crucial
+    vax_name_1_day = models.CharField(max_length=64)
+    symptom_after_vax_1_day = models.TextField()
+
+
+class Tuberculosis2005(Tuberculosis):
+    expected_vax_date_12_m = models.DateField()
+    vax_date_12_m = models.DateField()
+    vax_name_12_m = models.CharField(max_length=64)
+    symptom_after_vax_12_m = models.TextField()
+
+    expected_vax_date_7_y = models.DateField()
+    vax_date_7_y = models.DateField()
+    vax_name_7_y = models.CharField(max_length=64)
+    symptom_after_vax_7_y = models.TextField()
+
+    expected_vax_date_12_y = models.DateField()
+    vax_date_12_y = models.DateField()
+    vax_name_12_y = models.CharField(max_length=64)
+    symptom_after_vax_12_y = models.TextField()
+
+
+class HepatitisB(models.Model):
+    expected_vax_date_1_day = models.DateField()
+    vax_date_1_day = models.DateTimeField()  # hour are crucial
+    vax_name_1_day = models.CharField(max_length=64)
+    symptom_after_vax_1_day = models.TextField()
+
+    expected_vax_date_2_m = models.DateField()
+    vax_date_2_m = models.DateField()
+    vax_name_2_m = models.CharField(max_length=64)
+    symptom_after_vax_2_m = models.TextField()
+
+    expected_vax_date_7_m = models.DateField()
+    vax_date_7_m = models.DateField()
+    vax_name_7_m = models.CharField(max_length=64)
+    symptom_after_vax_7_m = models.TextField()
