f8ed01dab3e85ce4ef2c11569d7fa1ef24dce54a
m-sawula <marek.sawula@gmail.com>
2020-07-14 17:41:42 +0200

    added js file, hide and show details of workups and vax

---
+++ static/js/my_script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#btn-1').on('click', function (event) {
+        event.preventDefault();
+        $('#div-1').hide();
+    });
+
+
+    $('#btn-2').on('click', function (event) {
+        event.preventDefault();
+        $('#div-1').show();
+    });
+
+    $('#btn-3').on('click', function (event) {
+        event.preventDefault();
+        $('#div-2').hide();
+    });
+
+
+    $('#btn-4').on('click', function (event) {
+        event.preventDefault();
+        $('#div-2').show();
+    });
+
+
+});
